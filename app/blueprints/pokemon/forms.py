cd0e777caa16d8ebbf2b9407ec5cb29659244ce9
Sarah J <srjohnson4586@gmail.com>
2024-02-14 15:29:16 -0500

    html and css changes

--- app/blueprints/pokemon/forms.py
+++ app/blueprints/pokemon/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField, EmailField, ButtonField
 from wtforms.validators import DataRequired
 
 class PokemonInput(FlaskForm):
